50edcc6ab8987c017ea51f835e69119fd0d71bb5
Jan Rodan <jan.rodan@prodyna.com>
2020-09-28 22:00:47 +0200

    add underglow specific modes

--- keyboards/bm60rgb/keymaps/jrbm60rgb/keymap.c
+++ keyboards/bm60rgb/keymaps/jrbm60rgb/keymap.c
@@ -14,8 +14,16 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include QMK_KEYBOARD_H
+#include "lib/lib8tion/lib8tion.h"
 
 #define TAPPING_TERM 350
+
+static uint32_t underglow_mode;
+
+enum my_keycodes {
+  FOO = SAFE_RANGE,
+  JR_UG_MODE
+};
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
@@ -28,31 +36,41 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         RESET, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_DEL, 
         _______, KC_F14, KC_F15, LCTL(KC_UP), KC_F12, KC_F13, _______, _______, _______, _______, _______, _______, _______, _______, 
         _______, RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______,         _______, 
-        _______,    BL_TOGG, BL_DEC, BL_INC, BL_STEP, _______, _______, _______, _______, _______,      _______, KC__VOLUP, _______, 
+        _______,    JR_UG_MODE, _______, _______, _______, _______, _______, _______, _______, _______,      _______, KC__VOLUP, _______, 
         _______, _______, _______,                   _______,                    _______, _______, KC_MUTE, KC__VOLDOWN, _______),
+};
 
-};// This runs every matrix scan (every 'frame')
-// void rgb_matrix_indicators_user(void) {
-//     led_flags_t flags = rgb_matrix_get_flags();
+void rgb_matrix_layer_helper(uint8_t hue, uint8_t sat, uint8_t val, uint8_t mode, uint8_t speed, uint8_t led_type) {
+    HSV hsv = {hue, sat, val};
+    if (hsv.v > rgb_matrix_get_val()) {
+        hsv.v = rgb_matrix_get_val();
+    }
 
-//     // If we're in either keylight or underglow modes (but not both simultaneously)
-//     if (HAS_FLAGS(flags, LED_FLAG_KEYLIGHT) != HAS_FLAGS(flags, LED_FLAG_UNDERGLOW)) {
-//         rgb_matrix_set_color(32, 0, 0, 0);
-//     }
-// }
-
-// void keyboard_post_init_user(void) {
-//     // Enable the LED layers
-//     rgblight_layers = my_rgb_layers;
-// }
-
-// void rgb_matrix_layer_helper(uint8_t red, uint8_t green, uint8_t blue) {
-//     for (int i = 0; i < DRIVER_LED_TOTAL; i++) {
-//         if (HAS_FLAGS(g_led_config.flags[i], LED_FLAG_MODIFIER)) {
-//             rgb_matrix_set_color(i, red, green, blue);
-//         }
-//     }
-// }
+    switch (mode) {
+        case 1:  // breathing
+        {
+            uint16_t time = scale16by8(g_rgb_timer, speed / 8);
+            hsv.v         = scale8(abs8(sin8(time) - 128) * 2, hsv.v);
+            RGB rgb       = hsv_to_rgb(hsv);
+            for (uint8_t i = 0; i < DRIVER_LED_TOTAL; i++) {
+                if (HAS_FLAGS(g_led_config.flags[i], led_type)) {
+                    rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
+                }
+            }
+            break;
+        }
+        default:  // Solid Color
+        {
+            RGB rgb = hsv_to_rgb(hsv);
+            for (uint8_t i = 0; i < DRIVER_LED_TOTAL; i++) {
+                if (HAS_FLAGS(g_led_config.flags[i], led_type)) {
+                    rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
+                }
+            }
+            break;
+        }
+    }
+};
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     // static uint32_t key_timer;
@@ -86,6 +104,22 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
                 }
             }
             return false;
+        case JR_UG_MODE:
+            if (record->event.pressed) {
+                switch (underglow_mode) {
+                    case 1: {
+                        // breath
+                        rgb_matrix_layer_helper(HSV_MAGENTA, 1, rgb_matrix_config.speed, LED_FLAG_UNDERGLOW);
+                        underglow_mode = 0;
+                    } break;
+                    default: {
+                        // static
+                        rgb_matrix_layer_helper(HSV_CYAN, 0, rgb_matrix_config.speed, LED_FLAG_UNDERGLOW);
+                        underglow_mode = 1;
+                    } break;
+                }
+            }
+            return false;
         default:
             return true;  // Process all other keycodes normally
     }
@@ -98,35 +132,3 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 // https://github.com/qmk/qmk_firmware/blob/a4fd5e2491aa7213d15ef2ff3615b8eb75660e93/keyboards/crkbd/keymaps/rpbaptist/keymap.c
 // https://github.com/qmk/qmk_firmware/blob/caa70df816033c30dbbbf4c5a90d803c7bb1dfde/users/curry/rgb_matrix_user.c
 // per layer / per key https://www.reddit.com/r/olkb/comments/92w69w/heres_how_ive_implemented_perlayerperkey_color/
-
-// void rgb_matrix_layer_helper(uint8_t hue, uint8_t sat, uint8_t val, uint8_t mode, uint8_t speed, uint8_t led_type) {
-//     HSV hsv = {hue, sat, val};
-//     if (hsv.v > rgb_matrix_config.hsv.v) {
-//         hsv.v = rgb_matrix_config.hsv.v;
-//     }
-
-//     switch (mode) {
-//         case 1:  // breathing
-//         {
-//             uint16_t time = scale16by8(g_rgb_counters.tick, speed / 8);
-//             hsv.v         = scale8(abs8(sin8(time) - 128) * 2, hsv.v);
-//             RGB rgb       = hsv_to_rgb(hsv);
-//             for (uint8_t i = 0; i < DRIVER_LED_TOTAL; i++) {
-//                 if (HAS_FLAGS(g_led_config.flags[i], led_type)) {
-//                     rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
-//                 }
-//             }
-//             break;
-//         }
-//         default:  // Solid Color
-//         {
-//             RGB rgb = hsv_to_rgb(hsv);
-//             for (uint8_t i = 0; i < DRIVER_LED_TOTAL; i++) {
-//                 if (HAS_FLAGS(g_led_config.flags[i], led_type)) {
-//                     rgb_matrix_set_color(i, rgb.r, rgb.g, rgb.b);
-//                 }
-//             }
-//             break;
-//         }
-//     }
-// }
